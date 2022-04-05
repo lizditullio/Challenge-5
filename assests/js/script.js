be8e7494819f871de0068cfeed29902ff8e64d3b
Liz DiTullio <lizdit72296@gmail.com>
2022-04-05 19:34:25 -0400

    added comments to describe functionality

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,6 +1,6 @@
 var tasks = {};
 
-
+// display the currect day
 $("#currentDay").text("The current date is " + moment().format('MMMM Do YYYY'));
 
   $(".saveBtn").click(function(){
@@ -10,29 +10,16 @@ $("#currentDay").text("The current date is " + moment().format('MMMM Do YYYY'));
      localStorage.setItem(hour, task);
   });
 
-
-  function loadPlanner() {
-
-    $(".hour").each(function() {
-        var currHour = $(this).text();
-        var currTask = localStorage.getItem(currHour);
-
-        console.log(currTask);
-
-        if(currTask !== null) {
-            $(this).siblings(".description").val(currTask);
-        }
-    });
-}
-
-var whatTimeIsIt = parseInt(moment().format('h'));
+  // get the currect hour in military time and convert the time into a number
+var whatTimeIsIt = parseInt(moment().format('H'));
 console.log(whatTimeIsIt);
 
-
+// get the id for each time block and convert it into a number
 $(".time-block").each(function() {
     var taskTime = parseInt($(this).attr("id"));
     console.log(taskTime);
 
+    // compare the current time to the task time and give the block a corresponding class 
     if (whatTimeIsIt > taskTime) {
         $(this).addClass("past")
     } else if (taskTime === whatTimeIsIt) {
@@ -42,5 +29,18 @@ $(".time-block").each(function() {
     };
 });
 
+function loadPlanner() {
+    // checks to see if there is a task in local storage for each time block
+    $(".hour").each(function() {
+        var currHour = $(this).text();
+        var currTask = localStorage.getItem(currHour);
+
+        console.log(currTask);
+        // sets the description text to the task from storage as long as a task is available 
+        if(currTask !== null) {
+            $(this).siblings(".description").val(currTask);
+        }
+    });
+}
 
 loadPlanner();
